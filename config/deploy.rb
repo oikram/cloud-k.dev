4a1cb5e153df5e25ea6512fa2dd3639ffbdfb2fb
Marcio Machado <marcio.machado@nuvemk.com>
2011-03-30 12:37:31 +0100

    alterei a directoria do zend

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,48 +1,44 @@
-# Qual o nome da nossa aplicacao local ?
+# Must be set for the password prompt from git to work
+default_run_options[:pty] = true
+
+# The name of our application:
 set :application, "cloud-k.dev"
 
-# Qual o utilizador que se conecta ao servidor remoto?
+# What user will connect to the remote server?
 set :user, "nuvemk"
 
-# Onde está localizado o repositório ?
-set :repository, "git@github.com:oikram/cloud-k.dev.git"
+# Where is the repository ?
+set :repository, "git@github.com:oikram/cloud-k.dev.git" # My clone url
 
-# Qual o domínio do teu servidor de produção ?
+# What is the domain on "production" side?
 role :web, "dev.cloud-k.pt"
 
-# Qual a directoria remota que aloja o site?
+# Where should the files be deployed ?
 set :deploy_to, "/home/nuvemk/webapps/dev/"
 
-# O comando sudo é necessário para manipular os ficheiros no servidor remoto?
+# Do we need sudo?
 set :use_sudo, false
 
-# Que controlo de versão usamos no projeto?
+# What version control do we want to use?
 set :scm, :git
+
+# What password should I put here? Perhaps nothing since I'm using public keys?
+#set :scm_passphrase, "alcap0ne"  
+
+#If you’re using your own private keys for git you might want to tell Capistrano to use agent forwarding with this command. Agent forwarding can make key management much simpler as it uses your local keys instead of keys installed on the server
+ssh_options[:forward_agent] = true
+
 set :branch, 'master'
 
-# Password para fazer um deploy usando comandos git
-set :scm_passphrase, "alcap0ne"  
-
-# Como vão ser transferidos os ficheiros do projeto?
-# O deploy via :copy, fará com que o Capistrano clone o repositório, arquive-oe comprima-o e, depois, o transfira para o servidor remoto. Um modo mais eficiente do que o :copy seria o :remote_cache que faria o capistrano colocar apenas os últimos commits, em vez de transferir o projeto inteiro mas, para tal, o git teria de estar instalado no servidor.
-#set :deploy_via, :copy
+# Deploy via :remote_cache that will allow capistrano to deploy ONLY the last commits, instead of transfer the all project.
 set :deploy_via, :remote_cache
 
-# Manter a cache do repositório local para acelarar o processo?
-# Isto fará o Capistrano colocar em /tmp a cache do projeto.
-#set :copy_cache, true
-
-
-# Ignorar algum tipo de ficheiro específico aquando do envio para produção?
-#set :copy_exclude, %w(.git)
-
-# Criar uma task (chamada create_symlinks) que remova o /public/.htaccess que veio do nosso ambiente de desenvolvimento e que tem a seguinte instrução: SetEnv APPLICATION_ENV development e, mudamos por um outro (que estará numa pasta production, e que terá: SetEnv APPLICATION_ENV production
-
+# Create a task (named create_symlinks) that removes /public/.htaccess from our development environement, that has: SetEnv APPLICATION_ENV development and, change it by another .htaccess file prepared for production.
 task :create_symlinks, :roles => :web do
 run "rm #{current_release}/public/.htaccess"
 run "ln -s #{current_release}/production/.htaccess
 #{current_release}/public/.htaccess"
 end
 
-# Depois do deployment ter terminado com sucesso, chamos a nossa task (create_symlinks):
+# Call our task at the end:
 after "deploy:finalize_update", :create_symlinks
